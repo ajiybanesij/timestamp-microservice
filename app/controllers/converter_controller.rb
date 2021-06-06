c8270d59eb479a9b7d2770dfcb17828d2f1406c2
Ajiyba Nesij Korkmaz <ajiybanesij@hotmail.com>
2021-06-06 13:37:29 +0300

    controller edited

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -9,6 +9,7 @@ class ConverterController < ApplicationController
     if time_value.length == 13
       unix = time_value.to_i / 1000
       utc = DateTime.strptime(unix.to_s, '%s')
+      unix = unix.to_i * 1000
     elsif time_value.include? '-'
       utc = DateTime.strptime(time_value.to_s, '%Y-%m-%d')
       unix = utc.strftime('%s').to_i * 1000
@@ -18,6 +19,6 @@ class ConverterController < ApplicationController
       unix = unix.to_i * 1000
     end
 
-    render json: { "unix": unix, "utc": utc }
+    render json: { "unix": unix.to_s, "utc": utc }
   end
 end
